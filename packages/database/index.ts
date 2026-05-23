a9b37dfdce4e94f65336ec1de72e39a800c66960
Mayank Singla <Singlamayank10@gmail.com>
2026-05-24 00:16:24 +0530

    fix: add postinstall to generate prisma client on vercel

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -12,6 +12,5 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export { Prisma } from '@prisma/client' 
 export * from '@prisma/client'
 
